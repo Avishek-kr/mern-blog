24f25c7af1b62410140a37a0bda10d49e4741059
Avishek-kr <ak251997@gmail.com>
2024-07-27 23:38:41 +0530

    router added

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Signin from "./pages/Signin"
+import SignUp from "./pages/SignUp"
+import Dashboard from "./pages/Dashboard"
+import Projects from "./pages/Projects"
+
 function App() {
   return (
-    <h1 className="">App</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<Signin />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/projects' element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
