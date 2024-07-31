49ae9dae0c385ca0989b7fbb2f063a2e7d3f8e87
Avishek-kr <ak251997@gmail.com>
2024-07-31 21:49:34 +0530

    header navigation added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,15 +5,18 @@ import Signin from "./pages/Signin"
 import SignUp from "./pages/SignUp"
 import Dashboard from "./pages/Dashboard"
 import Projects from "./pages/Projects"
+import Header from "./components/Header"
 
 function App() {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/sign-in' element={<Signin />} />
         <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/dashboard' element={<Dashboard />} />
         <Route path='/projects' element={<Projects />} />
       </Routes>
     </BrowserRouter>
